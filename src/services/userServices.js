c69a1b58cda4993df6b5b3a473fc0ae363d5e4d9
Huỳnh Tiến Dĩ <103420884+DiHTCoder@users.noreply.github.com>
2023-12-10 15:20:22 +0700

    Add staff, update user status

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -11,6 +11,39 @@ const userServices = {
             },
         });
     },
+    addStaff(accessToken, username, password, displayName, role) {
+        const url = '/admin/user';
+        return axiosClient.post(
+            url,
+            {
+                username,
+                password,
+                displayName,
+                role,
+            },
+            {
+                headers: {
+                    accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            },
+        );
+    },
+    updateUser(accessToken, id, status) {
+        const url = `/admin/user/status/${id}`;
+        return axiosClient.patch(
+            url,
+            { status },
+            {
+                headers: {
+                    accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            },
+        );
+    },
 };
 
 export default userServices;
